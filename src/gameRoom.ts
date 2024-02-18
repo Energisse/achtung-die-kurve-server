e94cc190f9177426019b054ff2cac33ccd9f4173
Energisse <thomas.halvick@gmail.com>
2024-02-18 20:06:40 +0100

    frist playable "version"

--- src/gameRoom.ts
+++ src/gameRoom.ts
@@ -38,7 +38,22 @@ export default class GameRoom {
     /**
      * Static tick rate
      */
-    private staticTickRate = 1000 / 32
+    private staticTickRate = 1000 / 64
+
+    /**
+     * Width of the room
+     */
+    private width = 1000
+
+    /**
+     * Height of the room
+     */
+    private height = 1000
+
+    /**
+     * Interval of the game
+     */
+    private interval: NodeJS.Timeout | null = null
 
     constructor(Player: Player) {
         this.moderator = Player
@@ -68,21 +83,25 @@ export default class GameRoom {
         const socket = player.getSocket()
         socket.on('disconnect', () => {
             this.removePlayer(player)
-            io.to(this.id).emit('room', { players: this.getPlayerInfos() })
+            io.to(this.id).emit('leaderboard', this.getPlayerInfos())
         });
 
         socket.join(this.id);
 
-        io.to(this.id).emit('room', { players: this.getPlayerInfos() })
-        socket.on('room', (callback) => {
-            callback({ players: this.getPlayerInfos() })
+        io.to(this.id).emit('leaderboard', this.getPlayerInfos())
+        socket.on('leaderboard', (callback) => {
+            callback(this.getPlayerInfos())
         });
 
         socket.on('start', () => {
             if (this.players.length < 2) return
             if (this.moderator.getSocket().id !== player.getSocket().id) return
             io.to(this.id).emit('start')
-            setInterval(() => {
+            this.players.forEach((p) => {
+                p.revive()
+                p.setPositions([[Math.random() * this.width, Math.random() * this.height]])
+            })
+            this.interval = setInterval(() => {
                 this.tick()
             }, this.staticTickRate)
         })
@@ -93,13 +112,45 @@ export default class GameRoom {
      * Send the new positions of the players to the clients
      */
     private tick() {
-        const newPositions: Array<Array<number>> = []
+        const newPositions: Array<[number, number] | null> = []
         this.players.forEach((player) => {
+            if (!player.isAlive()) {
+                newPositions.push(null);
+                return
+            }
             player.tick()
-            newPositions.push(player.getPositions().at(-1)!)
+            newPositions.push(player.getPositions().at(-1)!);
+            const [x, y] = player.getPositions().at(-1)!
+            if (x < 0 || x > this.width || y < 0 || y > this.height) {
+                player.kill()
+            }
+            else if (player.getPositions().length > 2) {
+                this.players.forEach((p) => {
+                    if (player.collide(p)) {
+                        player.kill()
+                        if (p !== player) p.addPoints(1)
+                        else p.addPoints(-1)
+                        io.to(this.id).emit('leaderboard', this.getPlayerInfos())
+                    }
+                })
+            }
         })
         io.emit("tick", newPositions)
 
+        if (this.players.filter((p) => p.isAlive()).length < 2) {
+            clearInterval(this.interval!)
+            setTimeout(() => {
+                io.to(this.id).emit('start')
+                this.players.forEach((p) => {
+                    p.revive()
+                    p.setPositions([[Math.random() * this.width, Math.random() * this.height]])
+                })
+                this.interval = setInterval(() => {
+                    this.tick()
+                }, this.staticTickRate)
+            }, 3000)
+            io.emit("end")
+        }
     }
 
     /**
@@ -118,6 +169,8 @@ export default class GameRoom {
             isModerator: this.moderator?.getSocket().id === p.getSocket().id,
             id: p.getSocket().id,
             color: p.getColor(),
+            alive: p.isAlive(),
+            points: p.getPoints()
         }))
     }
 
--- src/player.ts
+++ src/player.ts
@@ -15,7 +15,7 @@ export default class Player {
     /**
      * Positions of the player
      */
-    private positions: number[][] = [[0, 0]];
+    private positions: Array<[number, number]> = [[0, 0]];
 
     /**
      * Direction of the player
@@ -31,6 +31,16 @@ export default class Player {
      * Color of the player
      */
     private color: string = '#ffffff'
+
+    /**
+     * Is the player alive
+     */
+    private alive: boolean = true
+
+    /**
+     * Points of the player
+     */
+    private points: number = 0
 
     /**
      * Constructor of the player
@@ -64,11 +74,44 @@ export default class Player {
      */
     public tick() {
         let lastPosition = this.positions.at(-1)!
-        let newPosition: Array<number> = []
         this.angle += this.direction
-        newPosition.push(lastPosition[0] + Math.cos(this.angle))
-        newPosition.push(lastPosition[1] + Math.sin(this.angle))
-        this.positions.push(newPosition)
+        this.positions.push([
+            lastPosition[0] + Math.cos(this.angle),
+            lastPosition[1] + Math.sin(this.angle)
+        ])
+    }
+
+    public collide(player: Player) {
+        let selfMargin = 0
+
+        if (this === player) selfMargin = 3
+
+        const x1 = this.positions.at(-1)![0]
+        const y1 = this.positions.at(-1)![1]
+        const x2 = this.positions.at(-2)![0]
+        const y2 = this.positions.at(-2)![1]
+
+        for (let index = 1; index < player.positions.length - selfMargin; index++) {
+            const x3 = player.getPositions().at(index)![0]
+            const y3 = player.getPositions().at(index)![1]
+            const x4 = player.getPositions().at(index - 1)![0]
+            const y4 = player.getPositions().at(index - 1)![1]
+
+            if (this.intersect(
+                x1, y1, x2, y2,
+                x3, y3, x4, y4
+            )) return true
+        }
+
+        return false
+    }
+
+    private intersect(x1: number, y1: number, x2: number, y2: number, x3: number, y3: number, x4: number, y4: number) {
+        const det = (x2 - x1) * (y4 - y3) - (x4 - x3) * (y2 - y1);
+        if (det === 0) return false;
+        const lambda = ((y4 - y3) * (x4 - x1) + (x3 - x4) * (y4 - y1)) / det;
+        const gamma = ((y1 - y2) * (x4 - x1) + (x2 - x1) * (y4 - y1)) / det;
+        return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
     }
 
     /**
@@ -89,9 +132,9 @@ export default class Player {
 
     /**
      * Get the positions of the player
-     * @returns {number[][]} The positions of the player
+     * @returns {Array<[number, number]> } The positions of the player
      */
-    public getPositions(): number[][] {
+    public getPositions(): Array<[number, number]> {
         return this.positions;
     }
 
@@ -109,5 +152,51 @@ export default class Player {
      */
     public setColor(color: string) {
         this.color = color
+    }
+
+    /**
+     * Set the positions of the player
+     * @param position The positions of the player
+     */
+    setPositions(position: Array<[number, number]>) {
+        this.positions = position;
+    }
+
+    /**
+     * Kill the player
+     */
+    public kill() {
+        this.alive = false
+    }
+
+    /**
+     * Revive the player
+     */
+    public revive() {
+        this.alive = true
+    }
+
+    /**
+     * Is the player alive
+     * @returns {boolean} True if the player is alive, false otherwise
+     */
+    public isAlive(): boolean {
+        return this.alive
+    }
+
+    /**
+     * Get the points of the player
+     * @returns {number} The points of the player
+     */
+    public getPoints(): number {
+        return this.points
+    }
+
+    /**
+     * Add points to the player
+     * @param {number} points The points to add
+     */
+    public addPoints(points: number) {
+        this.points += points
     }
 }
