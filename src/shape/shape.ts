248696c4d06e178c7034f6bd06075cf65854838c
Energisse <thomas.halvick@gmail.com>
2024-03-06 23:23:25 +0100

    Missing file :(

---
+++ src/shape/shape.ts
@@ -0,0 +1,10 @@
+import QuadTree from "../quadtree/quadtree";
+
+export default abstract class Shape {
+
+    private static idCounter = 0;
+
+    public id = Shape.idCounter++;
+
+    public abstract collide(other: Shape): boolean;
+}
